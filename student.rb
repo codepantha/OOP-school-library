6de7d9574d641115e757104e81f1a43943aeb422
codepanther <pezechigozirim@gmail.com>
2022-01-24 23:30:58 +0100

    Add the Student class

---
+++ student.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require File.expand_path('person.rb', __dir__)
+# Student class
+class Student < Person
+  def initialize(age, permission, name, classroom)
+    super(age, permission, name)
+    @classroom = classroom
+  end
+
+  def play_hookey
+    "¯\(ツ)/¯"
+  end
+end
