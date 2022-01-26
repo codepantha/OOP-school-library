140904da99314ceebda8fb98a741b1a94a1c60b6
codepanther <pezechigozirim@gmail.com>
2022-01-26 05:23:32 +0100

    Refactor the correct_name method to 1 line

--- corrector.rb
+++ corrector.rb
@@ -1,7 +1,6 @@
 # Corrector class
 class Corrector
   def correct_name(name)
-    name = name[0].upcase + name[1..name.length - 1]
-    name.length > 10 ? name[0..9] : name
+    name[0].upcase + name[1..9]
   end
 end
